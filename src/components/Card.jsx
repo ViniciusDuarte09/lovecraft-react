9548d277e820b66336e4bdad0e6e8a8a2b2ac0de
ViniciusDuarte09 <viniciusduartesant@gmail.com>
2026-06-21 11:52:49 -0300

    Tela de Loading

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,33 @@
-import { useEffect, useState } from "react"
-import { getAllCreatures } from "../services/creaturesService";
+import { useState } from "react"
 
-export default function Card({creatureData, isExpanded}){
+export default function Card({creatureData, isExpanded, onToggle}){
+
+  //Layout padrão do Card, exibido caso não seja selecionado
+  function CollapsedLayout(){
+    return(
+      <div style={{border: "3px solid pink", height: "150px", width: "200px"}}>
+        <h1>{creatureData?.name}</h1>
+        <img src={creatureData?.img[0]} alt={creatureData?.name} style={{height: "100px"}}/>
+      </div>  
+    )
+  }
   
-  const[expanded, setExpanded] = useState(false);
-  const[data, setData] = useState(creatureData);
+  //Layout Expandido do Card, exibe mais informações em um disposição diferente
+  function ExpandedLayout(){
+    return(
+        <div style={{border: "3px solid blue", height: "150px", width: "200px"}}>
+        <h1>{creatureData?.name}</h1>
+        <img src={creatureData?.img[0]} alt={creatureData?.name} style={{height: "100px"}}/>
+        <p>{creatureData?.overview}</p>
+      </div>  
+    )
+  }
 
-  return(
-    <div style={{border: "3px solid pink", height: "150px", width: "200px"}}>
-      <h1>{data?.name}</h1>
-      <img src={data?.img[0]} alt={data?.name} style={{height: "100px"}}/>
-
+  //Escolhe o layout com base na interação do usuário
+  return (
+    //Confere se o componente foi clicado e executa a "função toggle", criada na Página e que muda o ID expandido
+    <div onClick={onToggle}>
+      {isExpanded ? <ExpandedLayout/> : <CollapsedLayout/>}
     </div>
   )
 
--- src/pages/Creatures.jsx
+++ src/pages/Creatures.jsx
@@ -3,48 +3,75 @@ import { useEffect } from "react";
 import { getAllCreatures } from "../services/creaturesService";
 import Card from "../components/Card"
 
+import styles from "./Creatures.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons"
+
 function Creatures(){
 
+    //Estados dos dados
     const[data, setData] = useState(null);
-    const[filteredData, setFilteredData] = useState(null);
-    const[search, setSearch] = useState(null);
-    const[selectedCategoy, setSelectedCategory] = useState(null);
     const[loading, setLoading] = useState(true);
     const[error, setError] = useState(null);
+    const[expandadId, setExpandedId] = useState(null)
 
+    //Puxa os dados da API
     useEffect(() => {
         async function loadCreatures(){
             try{
+                //Usa a Service criada para pegar TODOS os dados
                 const data = await getAllCreatures();
                 setData(data);
             } catch (err){
+                //Exibe uma menssagem de erro caso falhe
                 setError("Erro ao carregar criaturas: " + err)
             } finally{
+                //Por ser uma função assíncrona, ao terminar, tira a tela de loading
                 setLoading(false);
             }
         }
 
         loadCreatures();
     }, [])
+
+    //Estados dos Filtros
+    const[filteredData, setFilteredData] = useState(null);
+    const[search, setSearch] = useState(null);
+    const[selectedCategoy, setSelectedCategory] = useState(null);
+
+    //Função que altera o ID do Card expandido
+    //Basicamente, isto é exevutado quando um card é clicado
+    function handleToggle(id){
+        //Arrow Function que confere se o ID presente em Expande ID é igual ao ID passado
+        // Se for, coloca nulo (Colapsando o card), se não for, coloca o ID (Expandindo o Card)
+        setExpandedId((prev) => prev === id ? null : id)
+    }
     
-    if(loading) return (<p>Carregando</p>)
+    if(loading) {
+        return (
+            <div className={styles.loading_countainer}>
+                <p className={styles.loading_text}>Consultando o Necronomicon...</p>
+                <FontAwesomeIcon icon={faCircleNotch} className={styles.faCircleNotch}/>
+            </div>
+        )
+    }
     if(error) return (<p>{error?.message ?? String(error)}</p>)
 
     return (
-        // <ul>
-        //     {data.map((card) =>{
-        //         return(
-        //             <li>
-        //                 <Card creatureData={card} key={card.id}/>
-        //             </li>
-        //         )
-        //     })}
-        // </ul>
+        <ul>
+            {data.map((card) =>{
+                return(
+                    <li>
+                        <Card creatureData={card} key={card.id} isExpanded={expandadId === card.id} onToggle={() => handleToggle(card.id)}/>
+                    </li>
+                )
+            })}
+        </ul>
 
-        <div>
-            <Card creatureData={data[0]}/>
-            <Card creatureData={data[1]}/>
-        </div>
+        // <div>
+        //     <Card creatureData={data[0]} isExpanded={expandadId === data[0].id} onToggle={() => handleToggle(data[0].id)}/>
+        //     <Card creatureData={data[1]} isExpanded={expandadId === data[1].id} onToggle={() => handleToggle(data[1].id)}/>
+        // </div>
     )
 }
 
